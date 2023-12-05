3f27cef0ba7ab14d181e293b51b93723c6f2a250
filonov.vanya@gmail.com <filonov.vanya@gmail.com>
2023-12-05 13:23:11 +0300

    Клопова Паттерн Builder

---
+++ PR5/BuilderBurger/IBurgerBuilder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PR5.BuilderBurger;
+using PR5.DBCon;
+
+namespace PR5.BuilderBurger
+{
+    public interface IBurgerBuilder
+    {
+        IBurgerBuilder AddCheese();
+    
+        IBurgerBuilder AddBacon();
+        
+        
+        IBurgerBuilder AddLetuce();
+        
+        
+        IBurgerBuilder AddTomato();
+        
+        IBurgerBuilder AddPickles();
+       
+        Burgers GetBurgers();
+        
+    }
+}
---
+++ PR5/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PR5.BuilderBurger;
+using PR5.DBCon;
+
+
+namespace PR5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Model1 model = new Model1();    
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BurgerBuilder burgerBuilder = new BurgerBuilder();
+            BurgerDirector burgerDirector = new BurgerDirector(burgerBuilder);
+
+            if (comboBoxBurger.SelectedItem.ToString() == "Бургер стандартный") 
+                burgerDirector.BuildDefault();
+
+            else 
+                burgerDirector.BuildWithBacon();
+            
+            try
+            {
+
+                model.Burgers.Add(burgerBuilder.GetBurgers());
+                model.SaveChanges();
+            }
+
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+
+            LoadData();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            comboBoxBurger.SelectedIndex = 0;
+            dataGridView1.DataSource = model.Burgers.ToList();
+        }
+    }
+}
